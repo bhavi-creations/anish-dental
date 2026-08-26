8223b85983b1d0599c55053d294138181343f7c9
PVSairam01 <sairampachipala00@gmail.com>
2026-08-26 12:59:04 +0530

    Added changes in sample1 page

---
+++ sample1.php
@@ -0,0 +1,68 @@
+<?php include 'header.php'; ?>
+  <style>
+   
+  </style>
+
+
+  <div class="container">
+    <!-- Main Stats Marquee Section -->
+    <div class="stats-card-wrapper py-2">
+      <div class="marquee-track">
+        
+        <!-- Original 4 Items -->
+        <div class="stat-item">
+          <i class="fa-solid fa-tooth stat-icon"></i>
+          <span class="stat-number">10000+</span>
+          <span class="stat-text">RCT's</span>
+        </div>
+
+        <div class="stat-item">
+          <img src="./assets/img/icons/dentalimplants_new.png" alt="" style="width: 50px; height: 50px;">
+          <span class="stat-number">100+</span>
+          <span class="stat-text">IMPLANTS</span>
+        </div>
+
+        <div class="stat-item">
+          <img src="./assets/img/icons/aligners-icon.png" alt="" style="width: 50px; height: 50px;">
+          <span class="stat-number">500+</span>
+          <span class="stat-text">ALIGNERS</span>
+        </div>
+
+        <div class="stat-item">
+          <i class="fa-solid fa-users stat-icon"></i>
+          <span class="stat-number">5000+</span>
+          <span class="stat-text">PTS</span>
+        </div>
+
+        <!-- Duplicated Items for Seamless Loop -->
+         <div class="stat-item">
+          <i class="fa-solid fa-tooth stat-icon"></i>
+          <span class="stat-number">10000+</span>
+          <span class="stat-text">RCT's</span>
+        </div>
+
+        <div class="stat-item">
+          <i class="fa-solid fa-screwdriver-wrench stat-icon"></i>
+          <span class="stat-number">100+</span>
+          <span class="stat-text">IMPLANTS</span>
+        </div>
+
+        <div class="stat-item">
+          <i class="fa-solid fa-teeth-open stat-icon"></i>
+          <span class="stat-number">500+</span>
+          <span class="stat-text">ALIGNERS</span>
+        </div>
+
+        <div class="stat-item">
+          <i class="fa-solid fa-users stat-icon"></i>
+          <span class="stat-number">5000+</span>
+          <span class="stat-text">PTS</span>
+        </div>
+
+      </div>
+    </div>
+  </div>
+
+
+
+<?php include 'footer.php';?>
